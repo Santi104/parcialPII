812b1c95c28efe52b2aba2eb304303f6765a09b4
Edi Santiago Monzon Franco <monzonedi05@gmail.com>
2025-03-03 20:54:08 -0500

    parcial programacion II

---
+++ Libro/src/main/java/com/mycompany/libro/Libro.java
@@ -0,0 +1,21 @@
+package com.mycompany.libro;
+import java.util.Scanner;
+
+public class Libro {
+    static public Scanner sc = new Scanner(System.in);
+    String titulo, autor;
+    int numEjemplares, numEjemplaresPrestados;
+    
+    public void libro(String titulo, String autor, int numEjemplares, int numEjemplaresPrestados) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numEjemplares = NU
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+    public boolean nuevo() {
+        return true;
+    }
+}
